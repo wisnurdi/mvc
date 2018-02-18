825318953f98a055e364f07cb216932d886523ed
Wisnu <wisnurdi@gmail.com>
2018-02-19 02:02:01 +0700

    Create config.php

---
+++ app/config.php
@@ -0,0 +1,16 @@
+<?php
+
+$config = [
+	// 'default_c'		=> 'site',		//default controller
+	// 'rute'			=> 'r',			//parameter default untuk get adalah r	
+	'appname' 		=> 'Aplikasi beta',	//nama aplikasi
+	'dbdriver'		=> 'mysql',		//driver database. Next: sqlite, postgresql
+	// 'dbhost'			=> 'localhost',		//nilai defaultnya 127.0.0.1
+	// 'dbuser'		=> 'root',			//nilai defaultnya root
+	'dbpass'	=> 'toor',			//nilai defaultnya blank
+	'dbname'		=> 'crudbuku',
+	'urlalias' => [
+		'about' => 'site/about',
+		'contact' => 'site/contact',
+	],
+];
